4ae94f7914f35346bd6722c08aba487452c78258
Solal Pirelli <solal.pirelli@gmail.com>
2019-04-11 15:55:46 +0200

    Fix bug with hc2 locations and improve passwords

--- hc2_export_teams.py
+++ hc2_export_teams.py
@@ -8,10 +8,11 @@ import requests
 def gen_password():
   password = ''
   for i in range(3):
-    password += random.choice('abcdefghijklmnopqrstuvwxyz')
+    # Avoid characters often confused: i, l, I, 1, o, O, 0, s, S, 5, B, 8
+    password += random.choice('abcdefghjkmnpqrtuvwxyz')
     password += random.choice('.!?')
-    password += random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    password += random.choice('0123456789')
+    password += random.choice('ACDEFGHJKLMNPQRTUVWXYZ')
+    password += random.choice('234679')
   return password
 
 def clean(s):
@@ -44,7 +45,7 @@ hc2_secret = input('HC2.ch secret: ')
 
 
 # Get the teams list
-hc2_req = requests.get('http://myhc2.azurewebsites.net/admin/approved?secret=' + hc2_secret)
+hc2_req = requests.get('http://my.hc2.ch/admin/approved?secret=' + hc2_secret)
 hc2_req.encoding = 'utf-8'
 hc2_soup = BeautifulSoup(hc2_req.text, 'html.parser')
 
@@ -54,6 +55,7 @@ rows = hc2_soup.findAll('tr')[1:]
 
 # Convert to teams
 teams = {}
+loc_idx = 0
 for idx, row in enumerate(rows):
   cells = row.findAll('td')
   team_name = clean(cells[0].text)
@@ -64,11 +66,12 @@ for idx, row in enumerate(rows):
       'name': team_name,
       'members': [],
       'category_id': dj_categ_pro if clean(cells[1].text) == 'professional' else dj_categ_student,
-      'location': locations[idx],
+      'location': locations[loc_idx],
       'password': gen_password(),
       'extra': []
     }
     teams[team_name] = team
+    loc_idx = loc_idx + 1
     print('Imported team ' + team_name)
 
   person_name = clean(cells[2].text)
